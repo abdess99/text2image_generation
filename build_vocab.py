b2712642cc60ec0369b4e5da299e4ea14dcf9480
abdess99 <ghilane1999@gmail.com>
2022-02-27 14:26:19 +0100

    feat/build our vocabulary

--- build_vocab.py
+++ build_vocab.py
@@ -1,6 +1,9 @@
 import argparse
+from pycocotools.coco import COCO
+from collections import Counter
 
-class Vocabulary (object):
+
+class Vocabulary(object):
     def __init__(self):
         self.word2idx = {}
         self.idx2word = {}
@@ -8,9 +11,9 @@ class Vocabulary (object):
 
     def add_word(self, word):
         if word not in self.word2idx:
-            self.word2idx[word]= self.idx
-            self.idx2word[self.idx]=word
-            self.idx+=1
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
 
     def get_idx(self, word):
         if word in self.word2idx.keys():
@@ -24,3 +27,28 @@ class Vocabulary (object):
         return len(self.word2idx)
 
 
+def build_vocab(captions_path, threshold):
+    coco = COCO(captions_path)
+    counter = Counter()
+    ids = coco.anns.keys()
+    for annotation_idx in ids:
+        caption = coco.anns[annotation_idx]['caption']
+        words = (caption
+                 .lower()
+                 .replace('.', '')
+                 .replace(',', '')
+                 .replace(':', '')
+                 .replace(';', '')
+                 .replace('!', '')
+                 .replace('?', '')
+                 )
+        words = words.split(' ')
+        counter.update(words)
+    words = [word for word, count in counter.items() if count >= threshold]
+    vocab = Vocabulary()
+    for word in words:
+        vocab.add_word(word)
+    vocab.add_word('<start>')
+    vocab.add_word('<end>')
+    vocab.add_word('<unknown>')
+    return vocab
